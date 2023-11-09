feb01b493787b4ff8ea2711f7ac01b2f6b424969
DavidCP-USC <125547745+DavidCP-USC@users.noreply.github.com>
2023-11-09 23:21:02 +0100

    Update create.sql
    Había un error en el archivo de create.sql

--- create.sql
+++ create.sql
@@ -2,7 +2,7 @@ create table users (
 	id serial primary key,
 	name varchar(255) not null unique,
 	password varchar(255) not null,
-	created_at timestamp not null default now(),
+	created_at timestamp not null default now()
 );
 
 create table friend_requests (
